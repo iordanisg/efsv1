2222653ce969c4a56219b53b7fd6da0ea021c63c
Iordanis Grigoriou <9437250+iordanisg@users.noreply.github.com>
2022-06-07 16:53:51 +0200

    Add custom-vars.el

---
+++ custom-vars.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" default))
+ '(history-length t)
+ '(package-selected-packages
+   '(forge evil-magit magit counsel-projectile projectile hydra evil-collection evil general helpful ivy-rich doom-themes which-key rainbow-delimiters doom-modeline counsel swiper command-log-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
